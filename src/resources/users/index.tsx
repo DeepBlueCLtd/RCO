35e171fe30628975ec7850ca8dc5317cd3ea8f33
Ian Mayo <ian@planetmayo.com>
2023-03-24 16:23:02 +0000

    use lazy declaration

--- src/resources/users/index.tsx
+++ src/resources/users/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Create, Edit, Show } from 'react-admin';
 import UserForm from '../../components/UserForm';
-import UserShow from './UserShow';
 
 const UserList = React.lazy(() => import('./UserList'));
+const UserShow = React.lazy(() => import('./UserShow'));
 
 const UserCreate = () => {
 	return (
