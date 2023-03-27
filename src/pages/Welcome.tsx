af37f527bfb66e4640b858f83baa33359e49d63b
Salony <salony@75way.com>
2023-03-27 19:01:34 +0530

    Revert welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import AppIcon from '../assets/rco_transparent.png';
 
 export default function Welcome(): React.ReactElement {
 	return (
@@ -12,8 +13,12 @@ export default function Welcome(): React.ReactElement {
 					height: '100vh',
 				}}
 			>
-				<Typography variant="h1">Welcome</Typography>
+				<img src={AppIcon} style={{
+					width: '228px',
+					height: '228px'
+				}} />
+				<Typography variant="h1">&nbsp;Welcome to RCO</Typography>
 			</Box>
 		</>
-	);
-}
+	)
+};
