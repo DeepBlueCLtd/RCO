a5c97fb3c4c671ebe12e9f29671df6c8f5e4cb01
Ian Mayo <ian@planetmayo.com>
2023-03-27 11:02:51 +0100

    Simplify

--- src/App.tsx
+++ src/App.tsx
@@ -53,8 +53,7 @@ function App(): React.ReactElement {
 						...(permissions === 'admin'
 							? [
 								<Resource key='users' icon={Person} name="users" {...users} />,
-								<Resource key='audit' name="audit" {...audit} />,
-								<Resource key='platforms' name="platforms" {...platforms} />
+								<Resource key='audit' name="audit" {...audit} />
 							]
 							: []),
 						<CustomRoutes key='routes'>
