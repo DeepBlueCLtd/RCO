17a5a3460ac9cb5b010f3b698dea5323d8dded68
Ian Mayo <ian@planetmayo.com>
2023-03-30 12:10:16 +0100

    fix + operator warning

--- src/providers/dataProvider/index.ts
+++ src/providers/dataProvider/index.ts
@@ -95,7 +95,8 @@ export const getDataProvider = async (): Promise<DataProvider<string>> => {
           const { data } = record
           const { id, year_of_receipt: year } = data
           const yearVal: string = year
-          const idVal: string = (id + 1).toLocaleString('en-US', {
+          const idCtr: number = id + 1
+          const idVal: string = idCtr.toLocaleString('en-US', {
             minimumIntegerDigits: 2,
             useGrouping: false
           })
