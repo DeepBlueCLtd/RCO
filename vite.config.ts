30058fb467019ab2b1c664e731d5d868b7cbf99f
TahaKhanAbdalli  <tahaKhanAbdalli@gmail.com>
2023-04-11 14:45:16 +0500

    env variable issues fixed for jest and original generateBatchId function tested

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import react from '@vitejs/plugin-react'
 import preval from 'vite-plugin-babel-macros'
+import EnvironmentPlugin from 'vite-plugin-environment'
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react(), preval()],
-  base: '/RCO/',
+  plugins: [react(), preval(), EnvironmentPlugin(['VITE_KEY'])],
+  base: '/RCO/'
 }
