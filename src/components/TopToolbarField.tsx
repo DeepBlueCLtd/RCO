4db5ba2f23e6dee6c6f89d64cae921a28bdb595c
Salony <salony@75way.com>
2023-04-18 14:55:11 +0530

    fixed codacy error

--- src/components/TopToolbarField.tsx
+++ src/components/TopToolbarField.tsx
@@ -2,17 +2,20 @@ import { type Theme } from '@mui/material'
 import { Show, type ShowProps, TextField } from 'react-admin'
 import { type ReactElement } from 'react'
 
-const sx = (theme: Theme) => ({
-  width: '150px',
-  fontWeight: 'bold',
-  height: '35px',
-  display: 'flex',
-  alignItems: 'center',
-  padding: '16px',
-  background: theme.palette.primary.main,
-  justifyContent: 'center',
-  color: `${theme.palette.common.white} !important` 
-})
+const sx = (theme: Theme) => {
+  const color: string = `${theme.palette.common.white} !important`
+  return {
+    width: '150px',
+    fontWeight: 'bold',
+    height: '35px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '16px',
+    background: theme.palette.primary.main,
+    justifyContent: 'center',
+    color
+  }
+}
 
 type Props = Omit<ShowProps, 'children'> & {
   source: string
