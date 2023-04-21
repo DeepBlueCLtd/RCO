2300e803045264dd5a90d24a35a5975e9f88bef0
Salony <salony@75way.com>
2023-04-21 12:32:18 +0530

    Fixed batch create issue

--- src/resources/batches/BatchForm.tsx
+++ src/resources/batches/BatchForm.tsx
@@ -15,7 +15,6 @@ const schema = yup.object({
   project: yup.number().required(),
   platform: yup.number().required(),
   organisation: yup.number().required(),
-  protectiveMarkingAuthority: yup.number().required(),
   maximumProtectiveMarking: yup.number().required(),
   remarks: yup.string().required()
 })
