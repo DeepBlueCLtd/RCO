c3f0b2f85701fe8549f1ed485f81fb70ac30f421
qajay97 <qaeng18@gmail.com>
2023-04-26 18:46:03 +0530

    added data for login test

---
+++ e2e/data/LoginTest/index.ts
@@ -0,0 +1,14 @@
+const ValidLoginCredentials = {
+  admin: {
+    username: 'ian',
+    password: 'admin'
+  },
+  user: {
+    username: 'jason',
+    password: 'user'
+  }
+}
+
+const invalidPassword = 'password'
+
+export { ValidLoginCredentials, invalidPassword }
