c29660f9e82eba0771bbb98b7c08c01950184181
qajay97 <qaeng18@gmail.com>
2023-04-27 15:23:44 +0530

    moved playwright config in root

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, devices } from '@playwright/test'
+export default defineConfig({
+  testDir: './e2e/tests',
+  fullyParallel: true,
+  forbidOnly: !(process.env.CI == null),
+  retries: process.env.CI != null ? 2 : 0,
+  workers: process.env.CI != null ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://deepbluecltd.github.io/',
+
+    trace: 'on',
+    headless: false
+  },
+
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] }
+    }
+  ]
+})
