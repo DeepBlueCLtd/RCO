b93d0ff9f90faa0f2b99b63fa315a4c86a362ff7
TahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2023-05-05 18:14:52 +0500

    self-sufficient tests written for batch resource

--- src/providers/dataProvider/dataprovider.test.ts
+++ src/providers/dataProvider/dataprovider.test.ts
@@ -3,7 +3,7 @@ import { getDataProvider } from '.'
 import { R_BATCHES } from '../../constants'
 import { DateTime } from 'luxon'
 
-const year: number = 2023
+const year: number = 2025
 
 interface NewBatch extends Omit<Batch, 'id'> {
   readonly id?: number
@@ -21,7 +21,7 @@ const generateDummyBatchForTesting = ({ name, id }: Props = {}): NewBatch => {
     startDate: DateTime.now().toFormat('yyyy-MM-dd'),
     endDate: DateTime.now().plus({ day: 1 }).toFormat('yyyy-MM-dd'),
     projectCode: '1',
-    batchNumber: 'V01/2023',
+    batchNumber: `V01/${year}`,
     yearOfReceipt: String(year),
     department: 1,
     project: 1,
@@ -37,18 +37,23 @@ const generateDummyBatchForTesting = ({ name, id }: Props = {}): NewBatch => {
 
 describe('CRUD operations on each resource', () => {
   let provider: DataProvider
+  let id: number
+  let batch: Batch
   beforeAll(async () => {
     provider = await getDataProvider(false)
   })
 
-  describe('CRUD Operation on Batch', () => {
-    let id: number
-    let batch: Batch
+  beforeEach(async () => {
+    if (id !== undefined)
+      await provider.delete(R_BATCHES, {
+        id
+      })
+  })
 
+  describe('CRUD Operation on Batch', () => {
     it('should create a batch', async () => {
-      const data = generateDummyBatchForTesting()
       const result = await provider.create<Batch>(R_BATCHES, {
-        data
+        data: generateDummyBatchForTesting()
       })
       id = result.data.id
       batch = result.data
@@ -57,17 +62,23 @@ describe('CRUD operations on each resource', () => {
     })
 
     it('should get the batch', async () => {
-      const data = generateDummyBatchForTesting({ id })
-      const result = await provider.getOne<Batch>(R_BATCHES, { id })
+      const createdBatch = await provider.create<Batch>(R_BATCHES, {
+        data: generateDummyBatchForTesting()
+      })
+      id = createdBatch.data.id
+      const result = await provider.getOne<Batch>(R_BATCHES, {
+        id
+      })
+      const shouldMatchBatch = generateDummyBatchForTesting({ id })
       const expectedResult = result.data
-      expect(expectedResult).toMatchObject(data)
+      expect(expectedResult).toMatchObject(shouldMatchBatch)
     })
 
     it('should update the batch', async () => {
-      const updatedValue = generateDummyBatchForTesting({
-        name: 'dummy-batch',
-        id
+      const createdBatch = await provider.create<Batch>(R_BATCHES, {
+        data: generateDummyBatchForTesting()
       })
+      id = createdBatch.data.id
       const result = await provider.update<Batch>(R_BATCHES, {
         id,
         previousData: batch,
@@ -76,18 +87,22 @@ describe('CRUD operations on each resource', () => {
           createdAt: DateTime.now().toFormat('yyyy-MM-dd')
         }
       })
+      const shouldMatchBatch = generateDummyBatchForTesting({
+        name: 'dummy-batch',
+        id
+      })
       const expectedResult = result.data
-      expect(expectedResult).toMatchObject(updatedValue)
+      expect(expectedResult).toMatchObject(shouldMatchBatch)
     })
 
     it('should delete the batch', async () => {
+      const created = await provider.create<Batch>(R_BATCHES, {
+        data: generateDummyBatchForTesting()
+      })
+      id = created.data.id
       await expect(
         provider.delete<Batch>(R_BATCHES, { id })
       ).resolves.toBeTruthy()
-    })
-
-    it('should try to get the deleted batch and fail', async () => {
-      await expect(provider.getOne<Batch>(R_BATCHES, { id })).rejects.toThrow()
     })
   })
 })
