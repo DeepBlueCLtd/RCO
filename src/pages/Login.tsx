4ef5544e006b41438772cf22e642397255514e25
Ian Mayo <ian@planetmayo.com>
2023-05-10 17:40:22 +0100

    mangle login page, so e2e fails

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -115,10 +115,10 @@ export default function Login(): React.ReactElement {
               margin='normal'
               required
               fullWidth
-              name='password'
+              name='password-fail'
               label='Password'
               type='password'
-              id='password'
+              id='password-fail'
               autoComplete='current-password'
             />
             <Button
