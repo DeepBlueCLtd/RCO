f043c127b7d4bf1123087906b7710cbce27db026
TahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2023-05-19 20:22:28 +0500

    removed after update from project lifecycle

--- src/providers/dataProvider/resource-callbacks/ProjectLifeCycle.ts
+++ src/providers/dataProvider/resource-callbacks/ProjectLifeCycle.ts
@@ -3,30 +3,15 @@ import {
   withCreatedByAt,
   extendLifeCycle
 } from '../dataprovider-utils'
-import { AuditType } from '../../../utils/activity-types'
-import {
-  type ResourceCallbacks,
-  type CreateResult,
-  type UpdateResult
-} from 'ra-core'
+import { type ResourceCallbacks, type CreateResult } from 'ra-core'
 import { R_PROJECTS } from '../../../constants'
 
-const lifeCycles = (
-  audit: AuditFunctionType
-): Omit<ResourceCallbacks<any>, 'resource'> => ({
+const lifeCycles = (): Omit<ResourceCallbacks<any>, 'resource'> => ({
   beforeCreate: async (record: CreateResult<Project>) => {
     return withCreatedByAt(record)
-  },
-  afterUpdate: async (record: UpdateResult<Project>) => {
-    await audit({
-      type: AuditType.EDIT,
-      resource: R_PROJECTS,
-      dataId: record.data.id
-    })
-    return record
   }
 })
 
 export default (audit: AuditFunctionType): ResourceCallbacks<any> => {
-  return extendLifeCycle(R_PROJECTS, audit, false, lifeCycles(audit))
+  return extendLifeCycle(R_PROJECTS, audit, false, lifeCycles())
 }
