8973216497f722a1e1fddfa2111ea95530f01d3c
qajay97 <qaeng18@gmail.com>
2023-06-15 20:05:03 +0530

    changed some selectors

--- e2e/tests/createbatch.spec.ts
+++ e2e/tests/createbatch.spec.ts
@@ -91,11 +91,9 @@ test.describe('CREATE BATCH', () => {
         'div.RaTabbedShowLayout-content span:nth-child(3) div:nth-child(1) span'
       )
     ).toHaveText(platformValue)
-    await expect(
-      page.locator(
-        'div.RaTabbedShowLayout-content span:nth-child(2) div:nth-child(2) span:nth-child(1)'
-      )
-    ).toHaveText(projectValue)
+    await expect(page.locator('//p[text()="Project"]/../span')).toHaveText(
+      projectValue
+    )
     await expect(
       page.locator('//div[@id="main-content"]//span[3]//div[2]/span')
     ).toHaveText(organisationText)
