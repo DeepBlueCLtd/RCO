d8d7adc6eb1621e3baf96621c9a77daf94ae5279
TahaKhanAbdalli <tahakhanabdalli@gmail.com>
2023-07-20 18:56:14 +0500

    user to be allowed only single user instead of multiple roles

--- src/providers/authProvider/permissions.ts
+++ src/providers/authProvider/permissions.ts
@@ -26,51 +26,9 @@ const permissions: Record<UserRoles, ResourcePermissions> = {
 
 export default permissions
 
-const generatePermission = (
-  resourcePermission: Permission,
-  mergedPermissions?: Permission
-): Permission => {
-  if (mergedPermissions == null) return resourcePermission
-
-  if (mergedPermissions.all === '*' || resourcePermission.all === '*') {
-    return { read: true, write: true, delete: true }
-  }
-
-  const read =
-    typeof resourcePermission.read !== 'undefined' && resourcePermission.read
-      ? true
-      : mergedPermissions.read
-  const write =
-    typeof resourcePermission.write !== 'undefined' && resourcePermission.write
-      ? true
-      : mergedPermissions.write
-  const deletePermission =
-    typeof resourcePermission.delete !== 'undefined' &&
-    resourcePermission.read === true
-      ? true
-      : mergedPermissions.delete
-
-  return { read, write, delete: deletePermission }
-}
-
-function mergedPermission(
-  permissions: ResourcePermissions[]
-): ResourcePermissions {
-  const results: ResourcePermissions = permissions[0]
-  permissions.slice(1).forEach((permission) => {
-    Object.entries(permission).forEach(([key, value]) => {
-      results[key] = generatePermission(value, results[key])
-    })
-  })
-  return results
-}
-
-export const getPermissionsByRoles = (
-  roles: UserRoles[]
-): ResourcePermissions => {
-  if (roles.length === 0) return permissions[roles[0]]
-  const userPermissions = roles.map((role) => permissions[role])
-  return mergedPermission(userPermissions)
+export const getPermissionsByRoles = (role: UserRoles): ResourcePermissions => {
+  const userPermissions = permissions[role]
+  return userPermissions
 }
 
 export const canAccess = (
