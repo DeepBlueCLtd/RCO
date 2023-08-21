2f2397df061396ce8ac97e238c22e5210c044032
TahaKhanAbdalli <tahakhanabdalli@gmail.com>
2023-08-21 16:56:27 +0500

    build failing fixed

--- src/providers/dataProvider/index.ts
+++ src/providers/dataProvider/index.ts
@@ -262,7 +262,7 @@ export const dataProvider = (apiUrl: string): DataProvider => ({
     // remove the id property
     const { id, ...editData } = params.data
 
-    return await axios.put(url, { fields: editData }).then((response) => {
+    return await axios.put(url, { fields: editData }).then(() => {
       return {
         data: { id: params.id, ...params.data }
       }
