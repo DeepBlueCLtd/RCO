ced22af879251271343c98909324e4e42b1d4a90
TahaKhanAbdalli <tahakhanabdalli@gmail.com>
2023-09-28 14:44:38 +0500

    rco served from soul extension

--- _extensions/api.js
+++ _extensions/api.js
@@ -1,3 +1,6 @@
+const path = require('path')
+const express = require('express')
+
 const getIp = {
   method: 'GET',
   path: '/api/ip',
@@ -6,6 +9,17 @@ const getIp = {
   }
 }
 
+const rcoClient = {
+  method: 'GET',
+  path: '/',
+  handler: (req, res) => {
+    const clientPath = path.join(__dirname, '../dist', 'index.html')
+    res.app.use(express.static(path.join(__dirname, '../dist')))
+    res.sendFile(path.join(clientPath))
+  }
+}
+
 module.exports = {
-  getIp
+  getIp,
+  rcoClient
 }
