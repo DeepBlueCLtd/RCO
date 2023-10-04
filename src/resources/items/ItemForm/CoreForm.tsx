5d408b70f1f27ab516761141e50403df453aadcc
TahaKhanAbdalli <tahakhanabdalli@gmail.com>
2023-10-04 12:23:47 +0500

    media type sorted in item add form

--- src/resources/items/ItemForm/CoreForm.tsx
+++ src/resources/items/ItemForm/CoreForm.tsx
@@ -95,7 +95,9 @@ const CoreForm = (props: Props): React.ReactElement => {
         TextFieldProps={{ ref: mediaTypeRef }}
         disabled={disabled}
         source='mediaType'
-        choices={mediaTypes.filter((item: Record<string, any>) => item.active)}
+        choices={mediaTypes
+          .filter((item: Record<string, any>) => item.active)
+          .sort((a, b) => a.id - b.id)}
         sx={sx}
       />
       <FlexBox alignItems='flex-start'>
