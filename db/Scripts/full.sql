e59e7ab45b58c9c815539970a072c4c1023163e1
Ian Mayo <ian@planetmayo.com>
2023-10-24 09:07:43 +0100

    remove `active` property from `user` table

--- db/Scripts/full.sql
+++ db/Scripts/full.sql
@@ -105,7 +105,6 @@ CREATE TABLE IF NOT EXISTS user (
        id INTEGER PRIMARY KEY,
        name TEXT NOT NULL,
        password TEXT NOT NULL,
-       active INTEGER NOT NULL,
        role TEXT NOT NULL,  /* Should be string Ex.: 'rco-user, 'rco-power-user' */
        staffNumber TEXT NOT NULL,
        departedDate TEXT,
