e3994bc64591d1eee4f9641af31135733ea4210c
tahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2023-12-18 17:53:55 +0500

    detail description of edit in audit log resolved

--- src/resources/users/index.tsx
+++ src/resources/users/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Create, Edit } from 'react-admin'
+import React, { useState } from 'react'
+import { Create, Edit, useEditContext } from 'react-admin'
 import UserForm from './UserForm'
 import * as constants from '../../constants'
 import useAudit from '../../hooks/useAudit'
 import { AuditType } from '../../utils/activity-types'
+import { getDifference } from '../../providers/dataProvider/dataprovider-utils'
 
 const UserList = React.lazy(async () => await import('./UserList'))
 const UserShow = React.lazy(async () => await import('./UserShow'))
@@ -21,6 +22,7 @@ const UserCreate = (): React.ReactElement => {
 const UserEdit = (): React.ReactElement => {
   const path: string = `/${constants.R_USERS}`
   const audit = useAudit()
+  const [prev, setPrev] = useState<User>()
 
   return (
     <Edit
@@ -29,8 +31,10 @@ const UserEdit = (): React.ReactElement => {
           await audit({
             resource: constants.R_USERS,
             activityType: AuditType.EDIT,
-            dataId: data.id ,
-            activityDetail: 'User edited',
+            dataId: data.id,
+            activityDetail: `{"Previous values": ${
+              prev ? JSON.stringify(getDifference(data, prev)) : 'User edited'
+            }}`,
             securityRelated: true,
             subjectResource: null,
             subjectId: null
@@ -40,16 +44,27 @@ const UserEdit = (): React.ReactElement => {
       mutationMode={constants.MUTATION_MODE}
       resource={constants.R_USERS}
       redirect={path}>
-      <UserForm isEdit />
+      <EditVal setPrev={setPrev} />
     </Edit>
   )
 }
 
+interface EditValType {
+  setPrev: React.Dispatch<React.SetStateAction<User | undefined>>
+}
+
+const EditVal = ({ setPrev }: EditValType): React.ReactElement => {
+  const { record } = useEditContext()
+  console.log(record)
+  setPrev(record)
+  return <UserForm isEdit />
+}
+
 const users = {
-  create: UserCreate,
   edit: UserEdit,
   list: UserList,
   show: UserShow
 }
 
 export default users
+export { UserCreate }
