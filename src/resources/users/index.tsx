9b5b468c30f8e3f6c71ee41c8fdf1d611781f8a6
tahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2023-12-18 11:25:20 +0500

    Audit log on edit to user table resolved

--- src/resources/users/index.tsx
+++ src/resources/users/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Create, Edit } from 'react-admin'
 import UserForm from './UserForm'
 import * as constants from '../../constants'
+import useAudit from '../../hooks/useAudit'
+import { AuditType } from '../../utils/activity-types'
 
 const UserList = React.lazy(async () => await import('./UserList'))
 const UserShow = React.lazy(async () => await import('./UserShow'))
@@ -18,9 +20,23 @@ const UserCreate = (): React.ReactElement => {
 
 const UserEdit = (): React.ReactElement => {
   const path: string = `/${constants.R_USERS}`
+  const audit = useAudit()
 
   return (
     <Edit
+      mutationOptions={{
+        onSuccess: (data: User) => {
+          audit({
+            resource: constants.R_USERS,
+            activityType: AuditType.EDIT,
+            dataId: data.id as number,
+            activityDetail: 'User edited',
+            securityRelated: true,
+            subjectResource: null,
+            subjectId: null
+          })
+        }
+      }}
       mutationMode={constants.MUTATION_MODE}
       resource={constants.R_USERS}
       redirect={path}>
