3ca99761fd5851d335bd0a6634e21de3826bce56
tahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2024-01-26 18:27:07 +0500

    delete removed from address edit.

--- src/resources/addresses/AddressForm.tsx
+++ src/resources/addresses/AddressForm.tsx
@@ -2,15 +2,17 @@ import { BooleanInput, DateInput, SimpleForm, TextInput } from 'react-admin'
 import { R_ADDRESSES } from '../../constants'
 import { DateTime } from 'luxon'
 import React from 'react'
+import EditToolBar from '../../components/EditToolBar'
 
 interface Props {
   show?: boolean
+  isEdit?: boolean
 }
 
 const sx = { width: '100%' }
 
 export default function AddressForm(props: Props): React.ReactElement {
-  const { show } = props
+  const { show, isEdit } = props
 
   const defaultValues: Partial<Address> = {
     createdAt: DateTime.now().toISO() ?? undefined,
@@ -22,7 +24,7 @@ export default function AddressForm(props: Props): React.ReactElement {
     <SimpleForm
       defaultValues={defaultValues}
       resource={R_ADDRESSES}
-      toolbar={showForm ? false : undefined}>
+      toolbar={isEdit ? <EditToolBar /> : false}>
       <TextInput sx={sx} disabled={show} multiline source='fullAddress' />
       <BooleanInput disabled={show} defaultValue={true} source='active' />
       {showForm && (
