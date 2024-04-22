fbc4d6498e5fbfe99117988ce55e1b08073aa75e
tahaKhanAbdalli <tahaKhanAbdalli@gmail.com>
2024-04-22 19:18:51 +0500

    now user role in _users_roles table are editable.

--- src/resources/users/UserForm.tsx
+++ src/resources/users/UserForm.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
   SimpleForm,
   TextInput,
   useEditContext,
   SelectInput,
-  DateTimeInput
+  DateTimeInput,
+  SaveButton
 } from 'react-admin'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
-import EditToolBar from '../../components/EditToolBar'
-import { Typography } from '@mui/material'
+
+import {
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  Typography
+} from '@mui/material'
 import { rolesOptions } from '../../utils/options'
 import FlexBox from '../../components/FlexBox'
 import { DateTime } from 'luxon'
+import axios from 'axios'
 const schema = yup.object({
   name: yup.string().required(),
   role: yup
@@ -25,6 +33,24 @@ const schema = yup.object({
     .nullable()
     .optional()
 })
+
+type UpdateUserRoleFunction = () => Promise<void>
+
+const CustomSaveButton: React.FC<{
+  updateUserRole: UpdateUserRoleFunction
+}> = ({ updateUserRole }) => (
+  <div style={{ padding: '10px 15px' }}>
+    <SaveButton
+      onClick={() => {
+        updateUserRole()
+          .then(() => {})
+          .catch((error) => {
+            console.error('Error updating user role:', error)
+          })
+      }}
+    />
+  </div>
+)
 
 export default function UserForm({ isEdit }: FormProps): React.ReactElement {
   const valueWithTenYears = DateTime.now()
@@ -49,11 +75,45 @@ export default function UserForm({ isEdit }: FormProps): React.ReactElement {
   }
   const { record } = useEditContext()
   const pageTitle = isEdit !== undefined ? 'Edit User' : 'Add new User'
+  const [selectUser, setSelectUser] = useState('')
+  const [userRoleId, setUserRoleId] = useState<any[]>([])
+
+  useEffect(() => {
+    const fetchData = async (): Promise<void> => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8000/api/tables/_users_roles/rows?_filters=user_id:${record.id}`
+        )
+        setUserRoleId(response.data.data)
+        setSelectUser(response.data.data[0]?.role_id || '')
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData().catch((error) => {
+      console.error('Error in fetchData:', error)
+    })
+  }, [])
+
+  const updateUserRole: UpdateUserRoleFunction = async (): Promise<void> => {
+    try {
+      const response = await axios.put(
+        `http://localhost:8000/api/tables/_users_roles/rows/${userRoleId[0]?.id}`,
+        { fields: { role_id: selectUser } }
+      )
+      console.log('User role updated successfully:', response.data)
+      return response.data
+    } catch (error) {
+      console.error('Error updating user role:', error)
+      throw error
+    }
+  }
 
   return (
     <SimpleForm
       record={{ ...record }}
-      toolbar={<EditToolBar />}
+      toolbar={<CustomSaveButton updateUserRole={updateUserRole} />}
       defaultValues={defaultValues}
       resolver={yupResolver(schema)}>
       <Typography variant='h5' fontWeight='bold'>
@@ -71,11 +131,29 @@ export default function UserForm({ isEdit }: FormProps): React.ReactElement {
         />
         <TextInput source='username' label='Username' sx={{ flex: 1 }} />
       </FlexBox>
+
       <DateTimeInput
         source='departedDate'
         label='Departure Date'
         sx={{ width: '50%' }}
       />
+
+      <FormControl variant='filled' sx={{ width: '50%' }}>
+        <InputLabel id='demo-simple-select-filled-label'>User Role</InputLabel>
+        <Select
+          labelId='demo-simple-select-filled-label'
+          id='demo-simple-select-filled'
+          value={selectUser}
+          onChange={(e) => {
+            setSelectUser(e.target.value)
+          }}>
+          <MenuItem value=''>
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={2}>RCO User</MenuItem>
+          <MenuItem value={3}>RCO Power User</MenuItem>
+        </Select>
+      </FormControl>
     </SimpleForm>
   )
 }
